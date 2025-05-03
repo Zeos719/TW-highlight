cd9f033ec3e453ba20971cbf98874a46c6be8d9c
Zeos719 <56189064+Zeos719@users.noreply.github.com>
2025-05-03 11:05:13 +0300

    Update Obuv_v1.js

--- Obuv_v1.js
+++ Obuv_v1.js
@@ -29,6 +29,7 @@ function Obuv_onCtrlEnter(e) {
 	if (e.ctrlKey && (e.keyCode == 192)) { // Ctrl ~
 	//if (e.ctrlKey) {
 		console.log('Obuv_onCtrlEnter:', e.keyCode);
+		Preset_RB(1); //Незначительно отличаются
 	}
 }
 
